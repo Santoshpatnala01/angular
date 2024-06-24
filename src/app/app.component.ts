2f3321839b111144dbbd9ebf7caf2df216b2b125
Netex Software <netexsoftware@Dars-iMac.local>
2024-06-24 10:48:37 -0400

    course update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+// import { HeaderComponent } from './header/header.component';
+// import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
 
 
 @Component({
     selector: 'app-root',
-    standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, RouterOutlet, UserComponent, TasksComponent]
 })
+
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string;
